1598121f0c9e4a7f1ad8577a2ef2422b2848467a
xaviza11 <xaviza11@gmail.com>
2026-07-25 14:12:20 +0200

    feat: add the skeleton of the dices

--- src/components/game/pyramid/DicePool.vue
+++ src/components/game/pyramid/DicePool.vue
@@ -2,22 +2,29 @@
 import DiceComponent from "@/components/game/pyramid/DiceComponent.vue";
 import type { Colors } from "@/types/game/Colors";
 
-defineProps<{
+const props = defineProps<{
   dice: {
     color: Colors;
     value: 1 | 2 | 3;
   }[];
 }>();
+
+const slots = Array.from({ length: 5 });
 </script>
 
 <template>
   <div class="dice-pool">
-    <DiceComponent
-      v-for="(die, index) in dice.slice(0, 5)"
+    <div
+      v-for="(_, index) in slots"
       :key="index"
-      :color="die.color"
-      :value="die.value"
-    />
+      class="dice-slot"
+    >
+      <DiceComponent
+        v-if="props.dice[index]"
+        :color="props.dice[index].color"
+        :value="props.dice[index].value"
+      />
+    </div>
   </div>
 </template>
 
@@ -28,5 +35,13 @@ defineProps<{
   align-items: center;
   justify-content: center;
   gap: var(--space-md);
+}
+
+.dice-slot {
+  width: 42px;
+  height: 42px;
+
+  border-radius: 8px;
+  border: 2px dashed rgb(0 0 0 / .25);
 }
 </style>
