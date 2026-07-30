e514ac511f93c66269f3762c72dc754e6ecaa741
xaviza11 <xaviza11@gmail.com>
2026-07-31 00:56:08 +0200

    refactor: delete unused imports

--- src/sockets/update/playerId.ts
+++ src/sockets/update/playerId.ts
@@ -1,5 +1,4 @@
 import { socket } from "@/sockets/socket";
-import type { Game } from "@/types/game/Game";
 
 export function onPlayerId(callback: (data: {playerId: string}) => void) {
   socket.on("playerId", (data) => {
--- src/sockets/update/playerTurn.ts
+++ src/sockets/update/playerTurn.ts
@@ -1,5 +1,4 @@
 import { socket } from "@/sockets/socket";
-import type { Game } from "@/types/game/Game";
 
 export function onPlayerTurn(callback: (data: string) => void) {
   socket.on("currentPlayer", (data) => {
